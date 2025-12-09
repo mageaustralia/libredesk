8cd40db25709782a58aede695f6bf4a5adf2fa7c
Abhinav Raut <abhinavrautcs@gmail.com>
2025-12-10 01:14:22 +0530

    set auth layout bg color to #fafafa

--- frontend/src/layouts/auth/AuthLayout.vue
+++ frontend/src/layouts/auth/AuthLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-background">
+  <div class="min-h-screen flex flex-col bg-[#F9F9F9]">
     <main class="flex-1 flex items-center justify-center p-4">
       <div class="w-full max-w-[450px]">
         <slot></slot>
