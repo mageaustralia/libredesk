8d2fb5748fe02ec9130529d98f89df79626988a1
Abhinav Raut <abhinavrautcs@gmail.com>
2025-12-27 14:06:38 +0530

    update help text for inbox

--- frontend/src/views/admin/inbox/InboxView.vue
+++ frontend/src/views/admin/inbox/InboxView.vue
@@ -6,7 +6,7 @@
 
     <template #help>
       <div class="native-html">
-        <p>Connect your email account directly via Google or Microsoft, or configure IMAP and SMTP settings manually.</p>
+        <p>Connect your Google or Microsoft email account, or configure IMAP and SMTP settings manually.</p>
         <p>Each added inbox creates a new email channel for receiving customer emails.</p>
         <p>
           <a href="https://docs.libredesk.io/configuration/connecting-inboxes" target="_blank">
