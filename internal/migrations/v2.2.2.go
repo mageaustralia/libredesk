ac8d4bb8853598f43cac9e039134d005d2a2ff0c
Jonathan Leroy <jonathan@relkian.com>
2026-05-15 09:21:59 +0200

    Migrate DB to the new locale format

---
+++ internal/migrations/v2.2.2.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/knadh/koanf/v2"
+	"github.com/knadh/stuffbin"
+)
+
+func V2_2_2(db *sqlx.DB, fs stuffbin.FileSystem, ko *koanf.Koanf) error {
+	langMap := map[string]string{
+		"da": "da-DK",
+		"de": "de-DE",
+		"en": "en-US",
+		"es": "es-ES",
+		"fa": "fa-IR",
+		"fr": "fr-FR",
+		"it": "it-IT",
+		"ja": "ja-JP",
+		"mr": "mr-IN",
+	}
+
+	for localeCode, localeRegion := range langMap {
+		_, err = db.Exec(fmt.Sprintf(`
+			UPDATE settings SET value = '"%s"'::jsonb, updated_at = now()
+			WHERE key = 'app.lang' AND value = '"%s"'::jsonb;
+		`, localeRegion, localeCode))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
