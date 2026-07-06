4388dd07cac59a6478415741f78d547c3daa0332
Matthew Campbell <mcampbell@mageaustralia.com.au>
2026-07-06 04:01:44 +0000

    deploy: 2026-07-06 04:01 auto-commit after deploy

--- frontend/src/components/editor/TextEditor.vue
+++ frontend/src/components/editor/TextEditor.vue
@@ -2,7 +2,7 @@
   <div class="editor-wrapper h-full overflow-y-auto" :class="{ 'pointer-events-none': disabled }">
     <BubbleMenu
       :editor="editor"
-      :tippy-options="{ duration: 100, maxWidth: 'none' }"
+      :tippy-options="{ duration: 100, maxWidth: 'none', appendTo: () => document.body }"
       v-if="editor"
       class="bg-background p-1 box will-change-transform"
     >
